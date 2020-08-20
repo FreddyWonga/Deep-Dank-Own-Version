abed281de3c649546ab3ad710cf557d004f9f552
FreddyWonga <69177764+FreddyWonga@users.noreply.github.com>
2020-08-20 16:02:49 +0800

    slash

---
+++ Combat/Assets/Camera_Follow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    // Created by David Borger
+    // https://www.youtube.com/watch?v=urNrY7FgMao Based off this script
+
+    public Transform PlayerTransform;
+
+    private Vector3 cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float Smoothfactor = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPos = PlayerTransform.position + cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, Smoothfactor);
+    }
+}
---
+++ Combat/Assets/Mouselook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouselook : MonoBehaviour
+{
+    // Created by David Borger
+    // Based off this script https://answers.unity.com/questions/805776/isometric-game-player-look-at-cursor.html
+    Ray Ray; 
+
+    RaycastHit RayHit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Cast a Ray to the Ground
+        Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // If Ray hits an object..
+        if (Physics.Raycast(Ray, out RayHit))
+        {
+            if (RayHit.transform.tag == "Ground")
+            {
+                Vector3 targetPosition = new Vector3(RayHit.point.x, transform.position.y, RayHit.point.z);
+                transform.LookAt(targetPosition);
+            }
+        }
+    }
+}
