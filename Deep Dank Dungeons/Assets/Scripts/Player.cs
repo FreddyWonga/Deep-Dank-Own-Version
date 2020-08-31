3ffae070f4ab1af5f20bcfe29e6ac2fff2037092
FreddyWonga <69177764+FreddyWonga@users.noreply.github.com>
2020-08-31 14:48:27 +0800

    Score

---
+++ Deep Dank Dungeons/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void health = 3f;
+    public void damage = 1f;
+    public void score = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
