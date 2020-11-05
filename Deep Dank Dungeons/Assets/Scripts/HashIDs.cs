c44b95f528cb91c8c82fa00f42fbab3890680281
FreddyWonga <69177764+FreddyWonga@users.noreply.github.com>
2020-11-05 15:33:33 +0800

    Hashing and stuff

--- Deep Dank Dungeons/Assets/Scripts/HashIDs.cs
+++ Deep Dank Dungeons/Assets/Scripts/HashIDs.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// geeksforgeeks.org/c-program-hashing-chaining/
+
+// dont include int main()
+
+
+
 public class HashIDs : MonoBehaviour
 {
     public int walkingBool;
