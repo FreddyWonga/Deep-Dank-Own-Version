950c0b0394edc498284b727a5779e8e6058a27f0
FreddyWonga <69177764+FreddyWonga@users.noreply.github.com>
2020-11-16 11:32:25 +0800

    Spectator stuff

---
+++ Spectator/Assets/Rating.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rating : MonoBehaviour
+{
+    public Text ratingText;
+
+    public static Rating instanceinstance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void UpdateRating(int currentRating)
+    {
+        ratingText.text = "Rating: " + currentRating.ToString();
+    }
+}
---
+++ Spectator/Assets/Stats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Stats : MonoBehaviour
+{
+    [SyncVar]
+    public int playerRating = 0;
+
+    [SyncVar]
+    public GameObject player;
+
+    public static Stats instance;
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+}
